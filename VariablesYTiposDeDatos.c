1d8ef6ffcf762d8db9c82c26165f3e69c3b23cdf
Marco <maphy1707@gmail.com>
2023-07-24 22:16:26 -0600

    Variables y tipos de datos en c

---
+++ VariablesYTiposDeDatos.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	char C = 'y';
+	int i = 20;
+	short s = -1;
+	unsigned int u = 25;
+	long l = 5932;
+	float f = 72.534;
+	double d = 12323.877658;
+	printf("%.2lf", d);
+	
+	return 0;
+}
